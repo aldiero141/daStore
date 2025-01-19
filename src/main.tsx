b8592e7ff6b30cf34d3056a079613ea4a4d890ef
Aldo Aldiero <rivelinoaldo@gmail.com>
2025-01-20 05:14:30 +0700

    feat: add error page

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import Dashboard from "./app/dashboard/index.tsx";
 import DashboardUsers from "./components/Dashboard/Users/index.tsx";
 import DashboardCategories from "./components/Dashboard/Categories/index.tsx";
 import DashboardProducts from "./components/Dashboard/Products/index.tsx";
+import ErrorPage404 from "./404.tsx";
 
 const queryClient = new QueryClient();
 
@@ -18,27 +19,33 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+    errorElement: <ErrorPage404 />,
   },
   {
     path: "/products",
     element: <Products />,
+    errorElement: <ErrorPage404 />,
   },
   {
     path: "/dashboard",
     element: <Dashboard />,
+    errorElement: <ErrorPage404 />,
     children: [
       { index: true, element: <Navigate to="/dashboard/users" replace /> },
       {
         path: "/dashboard/users",
         element: <DashboardUsers />,
+        errorElement: <ErrorPage404 />,
       },
       {
         path: "/dashboard/categories",
         element: <DashboardCategories />,
+        errorElement: <ErrorPage404 />,
       },
       {
         path: "/dashboard/products",
         element: <DashboardProducts />,
+        errorElement: <ErrorPage404 />,
       },
     ],
   },
